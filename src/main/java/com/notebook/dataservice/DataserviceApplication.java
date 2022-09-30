d8b243e2a71d82e730bf03b4357aee326dd0edcc
lschulz <lukasz.h.schulz@gmail.com>
2022-09-30 19:40:38 +0200

    configure swagger

--- src/main/java/com/notebook/dataservice/DataserviceApplication.java
+++ src/main/java/com/notebook/dataservice/DataserviceApplication.java
@@ -4,16 +4,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class DataserviceApplication {
 
-	private static Logger LOG = LoggerFactory
+	private static final Logger LOG = LoggerFactory
 			.getLogger(DataserviceApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(DataserviceApplication.class, args);
-
-
+	}
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.notebook.dataservice")).build();
 	}
 
 }
